f2a756ffe8913974321cf58aeb137c2371591439
Mehtap Aslan <mehtapmeko@gmail.com>
2026-08-19 18:53:52 +0300

    Add files via upload

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: mehaslan <mehaslan@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 17:58:51 by mehaslan          #+#    #+#             */
-/*   Updated: 2026/08/19 18:13:02 by mehaslan         ###   ########.fr       */
+/*   Updated: 2026/08/19 18:20:17 by mehaslan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,4 @@ t_list *ft_lstlast(t_list *lst)
 	while(lst->next != NULL)
 		lst = lst->next;
 	return(lst);
-}
-
-#include <stdio.h>
-int main()
-{
-	t_list *head;
-
-	head = ft_lstnew(ft_strdup("hello"));
-	ft_lstadd_back(&head ,ft_strdup("world"));
-	ft_lstadd_back(&head ,ft_strdup("merhaba"));
-	ft_lstadd_back(&head ,ft_strdup("dunya"));
-	
-	t_list *last;
-
-	last = ft_lstlast(head);
-	printf("%s\n", (char *)last->content);
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mehaslan <mehaslan@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 21:22:42 by mehaslan          #+#    #+#             */
-/*   Updated: 2026/08/19 15:18:31 by mehaslan         ###   ########.fr       */
+/*   Updated: 2026/08/19 18:44:30 by mehaslan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list *new_list;
 	t_list *new_node;
 	void *content;
-
+	
+	new_list = NULL;
 	if(!lst || !f || !del)
 		return (NULL);
 	while(lst)
@@ -35,27 +36,23 @@ t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return(new_list);	
 }
-void	del(void *s)
-{
-	free(s);
-}
 
 void	*f(void *s)
 {
 	int i = 0;
-	char *ss;
-	ss = ft_strdup((char *)s);
-	while (ss[i])
+	char *str;
+	str = ft_strdup((char*)s);
+	while (str[i])
 	{
-		ss[i] = ft_toupper(ss[i]);
+		str[i] = ft_toupper(str[i]);
 		i++;
 	}
-	return (ss);
+	return (str);
 }
 #include <stdio.h>
 int main()
 {
-    t_list  *mapped_list; // ft_lstmap'ten dönecek YENİ liste için
+    t_list  *mapped_list;
     t_list  *temp;
 
     t_list *s1 = ft_lstnew(ft_strdup("mehtap"));
@@ -63,23 +60,17 @@ int main()
     t_list *s3 = ft_lstnew(ft_strdup("suhan"));
     t_list *s4 = ft_lstnew(ft_strdup("gomec"));
 
-    // DÜZELTME 4: Düğümleri birbirine doğru şekilde bağlamak
     s1->next = s2;
     s2->next = s3;
     s3->next = s4;
 
-    // DÜZELTME 5: Sadece fonksiyonun adını (f) gönderiyoruz ve sonucu yeni listeye atıyoruz.
-    mapped_list = ft_lstmap(s1, f, del);
+    mapped_list = ft_lstmap(s1, f, free);
 
-    // DÜZELTME 6: Yeni listeyi yazdırmak için doğru bir döngü
     temp = mapped_list;
     while (temp != NULL)
     {
         printf("%s\n", (char *)temp->content);
         temp = temp->next;
     }
-    
-    // (İyi bir pratik: Program biterken hem orijinal hem yeni listeyi ft_lstclear ile temizlemelisin)
-    
     return (0);
 }
